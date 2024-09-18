a20efd42a045916a082a2c33debf9d0a311ad644
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-09-18 18:27:37 +0900

    [style]: banner title ui 구현

---
+++ src/components/Home/Banner/Title.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from '@/styles/home/banner/title.module.css';
+
+function Title() {
+  return (
+    <div className={style.title_wrapper}>
+      <h1 className={style.title}>sebells injisangjung</h1>
+    </div>
+  );
+}
+
+export default Title;
