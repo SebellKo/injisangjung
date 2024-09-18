5474bc82c02befd02c5c340779bd06725dc2a07a
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-09-18 18:30:54 +0900

    [style]: home 페이지 ui 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import style from '@/styles/home/home.module.css';
+import Header from '@/components/common/Header';
+import Banner from '@/components/Home/Banner/Banner';
+import Main from '@/components/Home/Main/Main';
 
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className={style.container}>
+      <Header></Header>
+      <Banner></Banner>
+      <Main></Main>
+    </div>
+  );
 }
 
 export default Home;
