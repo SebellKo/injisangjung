b5b681b2e0bf96bcb7d55e912fe4154064c4bf55
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-09-18 19:00:19 +0900

    [refactor]: categorylist, categorylist item 컴포넌트화

--- src/components/Home/Main/Main.tsx
+++ src/components/Home/Main/Main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import style from '@/styles/home/main/main.module.css';
 import PostList from './PostList';
+import CategoryList from './CategoryList';
 
 function Main() {
   return (
     <main className={style.main}>
       <PostList></PostList>
-      <div className={style.category_list_container}></div>
+      <CategoryList></CategoryList>
     </main>
   );
 }
