d54e20cf12248c1c4c8d772618bfcb978d8bab56
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-10-07 23:31:39 +0900

    [style]: 디자인 변경을 인한 전체 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
 import style from '@/styles/home/home.module.css';
-import Header from '@/components/common/Header';
-import Banner from '@/components/Home/Banner/Banner';
-import Main from '@/components/Home/Main/Main';
 
 function Home() {
-  return (
-    <div className={style.container}>
-      <Header></Header>
-      <Banner></Banner>
-      <Main></Main>
-    </div>
-  );
+  return <div className={style.container}></div>;
 }
 
 export default Home;
