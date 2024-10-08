c8aa97d9d0dc5cc86de072dd666c58088c43352a
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-10-09 04:03:47 +0900

    [feat]: markdown editor 추가

--- src/components/new-post/Editor.tsx
+++ src/components/new-post/Editor.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import MDEditor from '@uiw/react-md-editor';
 
 function Editor() {
-  return <div>Editor</div>;
+  const [value, setValue] = useState<string | undefined>('Hello');
+
+  return (
+    <div data-color-mode="light">
+      <MDEditor
+        visibleDragbar={false}
+        enableScroll={false}
+        height={800}
+        value={value}
+        onChange={setValue}
+      ></MDEditor>
+    </div>
+  );
 }
 
 export default Editor;
--- src/components/new-post/NewPost.tsx
+++ src/components/new-post/NewPost.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import style from '@/styles/new-post/main/new-post.module.css';
 import Banner from './Banner';
+import Editor from './Editor';
 
 function NewPost() {
   return (
     <div className={style.new_post}>
       <Banner></Banner>
+      <Editor></Editor>
     </div>
   );
 }
