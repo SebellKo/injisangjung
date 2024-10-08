4e5d925ce8279039e80e38f95fe1fcef8f22fc4a
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-10-08 23:08:26 +0900

    [style] post 버튼 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,6 +5,7 @@ function Header() {
   return (
     <div className={style.header}>
       <h2 className={style.logo}>injisangjung</h2>
+      <h2 className={style.new_post}>post</h2>
     </div>
   );
 }
