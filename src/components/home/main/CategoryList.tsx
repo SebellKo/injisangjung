46e2714e66bee939fe673e399eea0ed98c6f9ae7
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-10-08 19:14:58 +0900

    [style]: 카테고리 클릭시 언더라인 추가

--- src/components/home/main/CategoryList.tsx
+++ src/components/home/main/CategoryList.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import style from '@/styles/home/main/categorylist.module.css';
 
+const mock = [
+  {
+    title: 'All',
+  },
+  {
+    title: 'Tech',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+  },
+  {
+    title: 'Writing',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+  },
+  {
+    title: 'Daily',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+  },
+  {
+    title: 'Project',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+  },
+];
+
 function CategoryList() {
+  const [selected, setSelected] = useState<number>(0);
+
+  const handleClickCategory = (index: number) => {
+    setSelected(index);
+  };
+
   return (
     <ul className={style.category_list}>
-      <li className={style.category_item}>
-        <h3 className={style.category_name}>Tech</h3>
-        <p className={style.category_desc}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s,
-        </p>
-      </li>
-      <li className={style.category_item}>
-        <h3 className={style.category_name}>Writing</h3>
-      </li>
-      <li className={style.category_item}>
-        <h3 className={style.category_name}>Daily</h3>
-      </li>
-      <li className={style.category_item}>
-        <h3 className={style.category_name}>Project</h3>
-      </li>
+      {mock.map((category, index) => (
+        <li
+          className={style.category_item}
+          onClick={() => handleClickCategory(index)}
+        >
+          <h3
+            className={`${style.category_name} ${
+              index === selected ? style.active : ''
+            }`}
+          >
+            {category.title}
+          </h3>
+          {selected === index && index !== 0 && (
+            <p className={style.category_desc}>{category.description}</p>
+          )}
+        </li>
+      ))}
     </ul>
   );
 }
