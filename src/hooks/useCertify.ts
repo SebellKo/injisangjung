a155b4e6b53373713a918ae322d7c82a51a099da
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-10-12 22:21:36 +0900

    [feat] useCertify 커스텀훅 구현

---
+++ src/hooks/useCertify.ts
@@ -0,0 +1,19 @@
+const useCertify = () => {
+  const handleCertify = async (id: string, password: string) => {
+    const response = await fetch('/api/auth', {
+      method: 'POST',
+      body: JSON.stringify({
+        id: id,
+        password: password,
+      }),
+    });
+    const { accessToken } = await response.json();
+
+    sessionStorage.setItem('accessToken', accessToken);
+    sessionStorage.setItem('isAdmin', 'true');
+  };
+
+  return { handleCertify };
+};
+
+export default useCertify;
