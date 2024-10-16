007553ce5732b5478f1d90b79f8455a84c25729a
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-10-17 02:21:41 +0900

    [feat]: 에디터 내용 배너로 프랍 드릴링

--- src/components/new-post/NewPost.tsx
+++ src/components/new-post/NewPost.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import style from '@/styles/new-post/main/new-post.module.css';
 import Banner from './Banner';
 import Editor from './Editor';
 
 function NewPost() {
+  const [postValue, setPostValue] = useState<string | undefined>(
+    '내용을 입력해 주세요.'
+  );
   return (
     <div className={style.new_post}>
-      <Banner></Banner>
-      <Editor></Editor>
+      <Banner postValue={postValue}></Banner>
+      <Editor postValue={postValue} setPostValue={setPostValue}></Editor>
     </div>
   );
 }
