46d65485ea45ab5ef646ba9c3a5a4bf547bd3b06
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-10-21 19:40:14 +0900

    [feat]: post get 요청 구현

--- src/app/api/posts/[postId]/route.ts
+++ src/app/api/posts/[postId]/route.ts
@@ -1,1 +1,21 @@
-export async function GET() {}
+import { connectDB } from '@/db/db';
+import { ObjectId } from 'mongodb';
+import { NextRequest, NextResponse } from 'next/server';
+
+interface Params {
+  params: {
+    postId: string;
+  };
+}
+
+export async function GET(req: NextRequest, { params }: Params) {
+  const postId = params.postId;
+
+  const db = (await connectDB).db('injisangjung');
+
+  const result = await db
+    .collection('posts')
+    .findOne({ _id: new ObjectId(postId) });
+
+  return NextResponse.json({ result }, { status: 200 });
+}
