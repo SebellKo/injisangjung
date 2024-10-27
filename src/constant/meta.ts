b1d18aac03af3b84c9565025950c383424491690
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-10-27 17:58:05 +0900

    [refactor]: head 메타데이터 추가

---
+++ src/constant/meta.ts
@@ -0,0 +1,23 @@
+export const META = {
+  title: 'injisangjung',
+  siteName: 'injisangjung',
+  description: '안녕하세요. 고세종의 인지상정 입니다.',
+  keyword: [
+    '인지상정',
+    'injisangjung',
+    '블로그',
+    'blog',
+    '일상',
+    'daily',
+    '개발',
+    '프론트엔드',
+    'frontend',
+  ],
+  url: 'https://sebell.site',
+  //   googleVerification: 'xxx',
+  //   naverVerification: 'xxx',
+  ogImage: '/assets/images/preview_default.jpeg',
+  icons: {
+    icon: '/assets/favicon.svg',
+  },
+} as const;
